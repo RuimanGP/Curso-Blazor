e80d1920edd5e2376b7e4e0858ec44013e8adf97
Ruimán <ruimangp@gmail.com>
2023-02-02 19:38:28 +0100

    ADD Componentes (I)

---
+++ ToDoList/Componentes/ComponentesBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ToDoList;
+
+public class ComponentesBase : ComponentBase
+{
+    public string TextoComponentesBase { get; set; } = "Texto deasde clase ComponentesBase";
+
+}
---
+++ ToDoList/Componentes/PanelBody.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoList.Componentes;
+
+public class PanelBody
+{
+    public string? Texto { get; set; }
+    public string? Estilo { get; set;}
+}
