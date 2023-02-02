5dcfc03007aec4a6d9520b79e91f550f2b284c60
Ruimán <ruimangp@gmail.com>
2023-02-02 18:02:03 +0100

    Initial commit

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoList.Models;
+
+public class ToDoItem
+{
+    public int TodoItemID { get; set; }
+    public string? Titulo { get; set; }
+    public string? Contenido { get; set; }
+    public string? Responsable { get; set; }
+    public List<string> Etiquetas { get; set; } = new List<string>();
+    public DateOnly? FechaEntrega { get; set; }
+    public TimeOnly? HoraEntrega { get; set; }
+    public bool EstaCompletado { get; set; } = false;        
+}
