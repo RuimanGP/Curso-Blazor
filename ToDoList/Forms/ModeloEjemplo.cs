40e1357d8ccbe8fde86241499600f5b414d0f1e8
Ruimán <ruimangp@gmail.com>
2023-02-05 12:54:56 +0100

    ADD Formulario

---
+++ ToDoList/Forms/ModeloEjemplo.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.wwwroot.Forms;
+
+public class ModeloEjemplo
+{
+    [Required]
+    [StringLength(10, ErrorMessage = "Nombre es demasiado largo")]
+    public string? Nombre { get; set; }
+}
